54b232e9ba7ccb71206558ca1617c75891195aac
Supty <ayrinsupty@hotmail.com>
2023-04-03 20:59:20 +0600

    display ordered items

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
@@ -23,4 +24,14 @@ class Order extends Model
         'payment_mode',
         'payment_id'
     ];
+
+    /**
+     * Get all of the comments for the Order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class, 'order_id', 'id');
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
+use App\Models\ProductColor;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OrderItem extends Model
 {
@@ -18,4 +21,24 @@ class OrderItem extends Model
         'quantity',
         'price'
     ];
+
+    /**
+     * Get the product that owns the OrderItem
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+    /**
+     * Get the productColor that owns the OrderItem
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function productColor(): BelongsTo
+    {
+        return $this->belongsTo(ProductColor::class, 'product_color_id', 'id');
+    }
 }
